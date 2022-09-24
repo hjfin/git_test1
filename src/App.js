54427f221485215541f7fd7ae1ebc6bfca7352a4
hyeju1123 <hjjun1123@gmail.com>
2022-09-24 14:16:18 +0900

    change code in v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Change code in version 2
         </a>
       </header>
     </div>
